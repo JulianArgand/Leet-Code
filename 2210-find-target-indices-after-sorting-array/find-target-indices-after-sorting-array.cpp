64464469becb011e5b958fccb4fe01e7f53fee8a
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-05 11:21:53 +0500

    Time: 0 ms (100.00%) | Memory: 15.2 MB (15.04%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        
+        vector<int> arr;
+        sort(begin(nums), end(nums));
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] == target)
+            {
+                arr.push_back(i);
+            }
+           
+        }
+         return arr;
+    }
+};
