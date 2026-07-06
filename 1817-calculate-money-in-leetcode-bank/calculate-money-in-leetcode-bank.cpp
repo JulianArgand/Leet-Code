b6a38b365ac03daaecffabc06211e52a307bc06f
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-06 12:21:57 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.26%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int totalMoney(int n) {
         int sum = 0;
-       // int week = n/7;
+       
         for(int i =0 ;i<n ;i ++)
         {
             sum+= (i/7+1) + (i%7);
