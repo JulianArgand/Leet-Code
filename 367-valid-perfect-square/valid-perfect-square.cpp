0c0d89a1ce3bb0cf7560d176de26baa11a137bf1
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-07 12:18:19 +0500

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.90%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        if(num==0||num==1) return true;
-        int low = 1;
-        int high = num;
-        while(low <= high){
-            long long mid = low + (high-low)/2;
-            long long sqr = mid*mid;
-            if(sqr==num)return true;
-            else if(sqr>num)high = mid-1;
-            else low = mid+1;
+        if (num == 0 || num == 1) {
+            return true;
+        }
+
+        int start = 1;
+        int end = num;
+
+        while (start <= end) {
+            long long mid = start + (end - start) / 2;
+            long long sqr = mid * mid;
+
+            if (sqr == num) {
+                return true;
+            } else if (sqr > num) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
         }
         return false;
     }
