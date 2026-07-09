85fade2066077297a0a2f2b864d7f4a5aae138d0
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-09 10:26:23 +0500

    Time: 0 ms (100.00%) | Memory: 25.7 MB (96.94%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
-       // sort(nums.begin(), nums.end());
+       
         int count = 0;
         int i;
         for( i =0; i<nums.size(); i++)
