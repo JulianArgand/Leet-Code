9060954c3910b1ecc1070b176f5fb4fb321538be
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-15 07:59:29 +0500

    Time: 0 ms (100.00%) | Memory: 16.3 MB (79.25%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
         int n = arr.size();
-        int quat = n /4;
+        int quat = n/4;
 
-        for(int i = 0; i<n-quat; i++)
+        for(int i = 0; i<n - quat; i++)
         {
             if(arr[i] == arr[i + quat])
             {
