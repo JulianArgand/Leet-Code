86a0cee07d65c6ea72421820f94c633fb0558553
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-20 09:39:11 +0500

    Time: 1 ms (59.80%) | Memory: 10.4 MB (47.79%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for(int i : moves)
+        {
+            if(i == 'U')
+            {
+                y++;
+            }
+            else if(i == 'D')
+            {
+                y--;
+            }
+            else if(i == 'R')
+            {
+                x++;
+            }
+            else if(i == 'L')
+            {
+                x--;
+            }
+
+        }
+        return x == 0 && y == 0;
+    }
+};
