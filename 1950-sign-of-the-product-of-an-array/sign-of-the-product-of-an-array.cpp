375edf2bcdc44521db586aff9abd602884fac4c7
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-07-20 09:23:37 +0500

    Time: 0 ms (100.00%) | Memory: 13.7 MB (51.52%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+       int sign = 1;
+       for(int i : nums)
+       {
+            if(i == 0)
+            {
+                return 0;
+            }
+            else if(i < 0)
+            {
+                sign = -sign;
+            }
+            
+       }
+       return sign;
+    }
+};
