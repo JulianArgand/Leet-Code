50b59436e0965b7aaf44bbb3b7e3ce8f6c439c2a
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-08-04 10:41:08 +0500

    Time: 1 ms (60.57%) | Memory: 32.2 MB (91.09%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int first = nums[0];
+        int last = nums[nums.size()-1];
+        int  j = 0;
+        vector<int> ans;
+        for(int i = first; i<last; i++)
+        {
+            if(i != nums[j])
+            {
+                ans.push_back(i) ;
+            }
+            else
+            {
+            j++;
+            }
+        }
+        return ans;
+    }
+};
