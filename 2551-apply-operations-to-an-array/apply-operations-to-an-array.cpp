0e5d457aea91cda0075749ae516d049e169dd49d
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-08-09 11:55:18 +0500

    Time: 0 ms (100.00%) | Memory: 13.1 MB (91.55%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-
         for (int i = 0; i < nums.size() - 1; i++) 
         {
             if (nums[i] == nums[i + 1]) 
@@ -10,7 +9,6 @@ public:
                 nums[i + 1] = 0; 
             }
         }
-
         int index = 0;
         for (int i = 0; i < nums.size(); i++) 
         {
