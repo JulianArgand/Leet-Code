aeede1e6eec41909e5d7227e41cb9b234221526c
Muhammad Maaz <i253066@isb.nu.edu.pk>
2026-08-15 09:30:59 +0500

    Time: 0 ms (100.00%) | Memory: 14.2 MB (94.44%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         int n = mat[0].size();
+
         for(int i = 0; i<mat.size(); i++)
         {
             mat[i].push_back(i);
         }
-
+        
         sort(mat.begin(), mat.end());
-
         vector<int> ans(k);
+        //900submission
         for(int i = 0 ; i<k; i++)
         {
             ans[i] = mat[i][n];
